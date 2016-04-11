6c88bcf353c0e7f992174a457ea405cf2586a280
Marcel Melo <marcel.msmelo@gmail.com>
2016-04-11 14:56:13 -0300

    display:none em parts do form

--- ProjetoForma&EstiloDeAprendizagem/js/TransiçãoPerguntas.js
+++ ProjetoForma&EstiloDeAprendizagem/js/TransiçãoPerguntas.js
@@ -1,13 +1,19 @@
 var atual = 1;
 var nroParts = 9;
 
+window.onload = function(){
+  var i = 2;
+  for(i=2; i<=nroParts; i++){
+    document.querySelector(".part" + i).style.display = "none";
+  }
+}
+
 function avancar() {
     if (atual < nroParts) {
         document.querySelector(".part" + atual).style.display = "none";
         atual++;
         document.querySelector(".part" + atual).style.display = "block";
     }
-
 }
 
 function voltar() {
