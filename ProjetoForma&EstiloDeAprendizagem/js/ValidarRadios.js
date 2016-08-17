75e03bc6ae09d677ea02f943bbb2e0aaf9d6861d
SampaioJunior <juniorsam1@hotmail.com>
2016-08-17 12:03:52 -0300

    atualizando o meu projeto com o do yago

--- ProjetoForma&EstiloDeAprendizagem/js/ValidarRadios.js
+++ ProjetoForma&EstiloDeAprendizagem/js/ValidarRadios.js
@@ -11,7 +11,7 @@ for(let i = 0; i< radios.length; i++){
 
 window.addEventListener('load', ()=>{
     var radios = document.querySelectorAll(".questao2");
-for(let i = 0; i< radios.length; i++){zz
+for(let i = 0; i< radios.length; i++){
     var radio = radios[i];
     radio.addEventListener('change',verificaRadioMarcado);
 }
